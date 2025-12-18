04c7e2663180b7da30c4e86d683cd005d20cd53b
Amil Ali <amil13985@gmail.com>
2025-12-18 12:15:42 +0530

    feat: add and integrate `get_date` tool for retrieving relative dates.

--- google_adk_local/normal_agent/agent.py
+++ google_adk_local/normal_agent/agent.py
@@ -4,9 +4,28 @@ from dotenv import load_dotenv
 from google.adk import Agent
 from google.adk.runners import InMemoryRunner
 from google.genai import types
+from datetime import datetime, timedelta
 
 # Load environment variables from .env file
 load_dotenv()
+
+## Tool callbacks
+def get_date(x_days_from_today:int):
+    """
+    Retrieves a date for today or a day relative to today.
+
+    Args:
+        x_days_from_today (int): how many days from today? (use 0 for today)
+
+    Returns:
+        A dict with the date in a formal writing format. For example:
+        {"date": "Wednesday, May 7, 2025"}
+    """
+
+    target_date = datetime.today() + timedelta(days=x_days_from_today)
+    date_string = target_date.strftime("%A, %B %d, %Y")
+
+    return {"date": date_string}
 
 # Basic configuration
 # Ensure you have the GOOGLE_API_KEY environment variable set.
@@ -21,7 +40,9 @@ root_agent = Agent(
     model=model_name,
     name="helpful_assistant",
     instruction="You are a helpful and friendly AI assistant. Keep answers concise.",
+    tools=[get_date]
 )
+
 
 # Force using Google AI API (Local/Non-Vertex)
 # os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "0"
